c60fd0ce0a0548335915588f112bcbfd03a39411
Keoul Patel <patelkeoulron@gmail.com>
2019-10-24 22:00:04 +0530

    Implemented responsive design

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function dropinfo1() {
-    var x = document.getElementById("info1");
+    var x = document.getElementById("info1-mob");
     if (x.className.indexOf("w3-drop-1") == -1) {
       x.className += "w3-drop-1";
-      var y = document.getElementById("info2");
-      var z = document.getElementById("info3");
+      var y = document.getElementById("info2-mob");
+      var z = document.getElementById("info3-mob");
       y.className = y.className.replace("w3-drop-2","");
       z.className = z.className.replace("w3-drop-3","");
     } else { 
@@ -11,11 +11,11 @@ function dropinfo1() {
     }
   }
   function dropinfo2() {
-    var x = document.getElementById("info2");
+    var x = document.getElementById("info2-mob");
     if (x.className.indexOf("w3-drop-2") == -1) {
       x.className += "w3-drop-2";
-      var y = document.getElementById("info1");
-      var z = document.getElementById("info3");
+      var y = document.getElementById("info1-mob");
+      var z = document.getElementById("info3-mob");
       y.className = y.className.replace("w3-drop-1","");
       z.className = z.className.replace("w3-drop-3","");
     } else { 
@@ -23,11 +23,11 @@ function dropinfo1() {
     }
   }
   function dropinfo3() {
-    var x = document.getElementById("info3");
+    var x = document.getElementById("info3-mob");
     if (x.className.indexOf("w3-drop-3") == -1) {
       x.className += "w3-drop-3";
-      var y = document.getElementById("info2");
-      var z = document.getElementById("info1");
+      var y = document.getElementById("info2-mob");
+      var z = document.getElementById("info1-mob");
       y.className = y.className.replace("w3-drop-2","");
       z.className = z.className.replace("w3-drop-1","");
     } else { 
